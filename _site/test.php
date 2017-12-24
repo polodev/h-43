e07d0e201e489a21672b6b263006bbbae71eea98
Polo Dev <polodev10@gmail.com>
2017-12-25 02:56:39 +0600

    added class 48

--- _site/test.php
+++ _site/test.php
@@ -1,24 +1,5 @@
-<?php
-require 'vendor/autoload.php';
+<?php 
 
-// insert people data
-Person::insert([
-  "name" => "tofael",
-  "email" => "tofael6@gmail.com"
-]);
-
-// read
-$person = Person::all();
-print_r($person);
-
-//update 
-$sumon = Person::find(2);// $person = Person::all();
-// print_r($person);
-$sumon->update([
-  "name" => "mamun",
-  "email" => "mamun@gmail.com"
-]);
-
-//delete 
-Person::find(2)->delete();
-
+class Hello {
+  
+}
